2981e97b549b8770be62f2db0fc681bdf3338b2e
melitroncoso <68672246+melitroncoso@users.noreply.github.com>
2026-04-29 16:02:51 -0300

    test: C1 longitud minima , feat: validar longitud

--- src/validarPassword.js
+++ src/validarPassword.js
@@ -1,5 +1,11 @@
 function validarPassword(password, usernameOpcional) {
-  return { esValida: false, errores: [] };
+  const errores = [];
+
+  if (password.length < 8) {
+    errores.push("Debe tener al menos 8 caracteres");
+  }
+
+  return { esValida: errores.length === 0, errores };
 }
 
 module.exports = { validarPassword };
--- test/validarPassword.test.js
+++ test/validarPassword.test.js
@@ -1,7 +1,7 @@
-const { validarPassword } = require('../src/validarPassword');
+const { validarPassword } = require("../src/validarPassword");
 
-test('C1 - contraseña corta retorna error de longitud', () => {
-  const resultado = validarPassword('Ab1!');
+test("C1 - contrasenia corta retorna error de longitud", () => {
+  const resultado = validarPassword("Ab1!");
   expect(resultado.esValida).toBe(false);
-  expect(resultado.errores).toContain('Debe tener al menos 8 caracteres');
+  expect(resultado.errores).toContain("Debe tener al menos 8 caracteres");
 });
